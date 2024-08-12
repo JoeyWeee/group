9b64322f25acc4499ea549650526e07b83eca9b9
JoeyWeee <jiujiedexiaohuozi@gmail.com>
2024-08-12 16:26:52 -0400

    fixed 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const { PORT = 3000} = process.env;
 
 export default defineConfig({
- plugins: [react()],
- server:{
-     proxy:{
-     '/api':{
-     target:`http://localhost:${PORT}`,
-     changeOrigin: true,
-     },
-     '/auth': {
-     target:`http://localhost:${PORT}`,
-     changeOrigin: true,
-     },
-     },
+  plugins: [react()],
+  server: {
+    host: true,
+    port: process.env.PORT || 5173,
+    proxy: {
+      '/api': {
+        target: `http://localhost:3000`, // 确保此端口与后端服务一致
+        changeOrigin: true,
+      },
+      '/auth': {
+        target: `http://localhost:3000`,
+        changeOrigin: true,
+      },
     },
-    
- build: {
- manifest: true,
- rollupOptions: {
- input: "./src/main.jsx",
- outDir:'../dist/app',
- },
- },
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+      outDir: 'dist/app', // 确保这是一个有效路径
+    },
+  },
 });
