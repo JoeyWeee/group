2e6de20d0e0690232157d157eb6ff22d40a3149e
JoeyWeee <146883373+JoeyWeee@users.noreply.github.com>
2024-08-12 21:46:13 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI || "mongodb+srv://joeyweee:520520Shi@cluster0.qhnik17.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject',
     apiBaseUrl: process.env.NODE_ENV === 'production' ? 'https://jaxz-group.onrender.com' : 'http://localhost:3000'
