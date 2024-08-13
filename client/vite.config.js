3a6d5c1767fb34a3490f7c224767e835c0381ecf
JoeyWeee <146883373+JoeyWeee@users.noreply.github.com>
2024-08-12 22:12:39 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,28 +5,28 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: process.env.PORT || 5173,
+    port: import.meta.env.PORT || 5173,
     proxy: {
       '/api': {
-        target: `http://localhost:${process.env.BACKEND_PORT || 3000}`, // 确保与 Express 端口一致
+        target: `http://localhost:${import.meta.env.BACKEND_PORT || 3000}`, // 确保与 Express 端口一致
         changeOrigin: true,
       },
       '/auth': {
-        target: `http://localhost:${process.env.BACKEND_PORT || 3000}`,
+        target: `http://localhost:${import.meta.env.BACKEND_PORT || 3000}`,
         changeOrigin: true,
       },
       '/products': {
-        target: `http://localhost:${process.env.BACKEND_PORT || 3000}`,
+        target: `http://localhost:${import.meta.env.BACKEND_PORT || 3000}`,
         changeOrigin: true,
       }, 
-    },  
+    },
   },
   build: {
     manifest: true,
     rollupOptions: {
       input: "./src/main.jsx",
       output: {
-        dir: 'dist/app', 
+        dir: 'dist/app',
       },
     },
   },
